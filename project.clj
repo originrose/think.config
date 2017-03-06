6551751843c9b3f662fba2b6f630be6168c15ef3
Charles Gruenwald <charles@thinktopic.com>
2017-03-06 16:13:36 -0700

    Version 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.config "0.2.1-SNAPSHOT"
+(defproject thinktopic/think.config "0.2.1"
   :description "A configuartion library."
   :url "http://github.com/thinktopic/think.config"
 
