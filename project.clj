be89f48929035789f53202b2566c3b74e2d7f092
Harold <hhausman@gmail.com>
2017-05-08 15:50:23 -0600

    Version 0.2.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.config "0.2.8"
+(defproject thinktopic/think.config "0.2.9-SNAPSHOT"
   :description "A configuartion library."
   :url "http://github.com/thinktopic/think.config"
 
