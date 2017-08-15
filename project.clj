a73fce895bb7943f7c32a0a371ae77e2df6cb887
Charles Gruenwald <charles@thinktopic.com>
2017-08-15 11:07:39 -0600

    [core] Better handling of complex types.

--- project.clj
+++ project.clj
@@ -11,7 +11,10 @@
 
   :profiles {:test {:resource-paths ["test/resources"]
                     :env {:overwrite "80"
-                          :env-config-overwrite "true"}}}
+                          :env-config-overwrite "true"
+                          :complex-type-env-overwrite-map "{:a 1 :b 3}"
+                          :complex-type-env-overwrite-vec "[:c :b :a]"
+                          :complex-type-env-overwrite-seq "(:c :b :a)"}}}
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
